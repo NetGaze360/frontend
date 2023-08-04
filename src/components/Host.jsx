044061eb3de1616243d499cb219fa8459fba69e5
d4saher <daniel.sanchez.hernandez@estudiantat.upc.edu>
2023-08-04 15:45:43 +0200

    Drag & Drop

--- src/components/Host.jsx
+++ src/components/Host.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 const HostContainer = styled.div`
-  /* Aquí puedes agregar los estilos que necesites */
+  background-color: #fff;    color: ${(props)=>props.theme.text};
+  background: ${(props)=>props.theme.bg};  border: 1px solid #e5e5e5;
+  border-radius: 10px;
+  padding: 5px;
+  margin-bottom: 5px;
+  width: 100%;; 
+  cursor: pointer;
 `;
 
-const Host = ({ hostname, ip, description, brand }) => (
-  <HostContainer>
-    <h2>{hostname}</h2>
-    <p>{ip}</p>
-    <p>{description}</p>
-    <p>{brand}</p>
-  </HostContainer>
-);
+const Host = ({ _id, hostname, ip, description, brand }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({id: _id});
+  
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+
+    <HostContainer ref={setNodeRef} style={style}
+      {...listeners}
+      {...attributes}
+      >
+      <h2>{hostname}</h2>
+      <div className='hostInfo'>
+      <p>{ip}</p>
+      <p>{description}</p>
+      <p>{brand}</p>
+      </div>
+
+    </HostContainer>
+  );
+};
 
 export default Host;
--- src/pages/Hosts.jsx
+++ src/pages/Hosts.jsx
@@ -1,24 +1,52 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Host from '../components/Host';
+import { DndContext, closestCenter} from '@dnd-kit/core';
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 
 export const Hosts = () => {
     const [hosts, setHosts] = useState([]);
 
     useEffect(() => {
         // Aquí debes sustituir 'http://localhost:5000/hosts' por la URL de tu API
-        fetch(import.meta.env.MONGO_URI + '/hosts')
+        console.log(import.meta.env.VITE_API_URL + '/hosts');
+        fetch(import.meta.env.VITE_API_URL + '/hosts')
             .then(response => response.json())
             .then(data => setHosts(data))
             .catch(err => console.error(err));
     }, []); // El array vacío significa que este efecto se ejecutará sólo una vez, justo después de que el componente se monte
 
+    const handleDragEnd = (event) => {
+        const {over, active} = event;
+        if (over && active.id !== over.id) {
+            const overIndex = hosts.findIndex(host => host._id === over.id);
+            const activeIndex = hosts.findIndex(host => host._id === active.id);
+            const newHosts = [...hosts];
+            newHosts.splice(overIndex, 0, newHosts.splice(activeIndex, 1)[0]);
+            setHosts(newHosts);
+        }
+    };
+
     return (
         <Container>
-        <h1>Hosts</h1>
-        {hosts.map(host => (
-            <Host key={host._id} {...host} />
-        ))}
+            <h1>Hosts</h1>
+            <DndContext
+                collisionDetection={closestCenter}
+                onDragEnd={handleDragEnd}
+            >
+                <SortableContext 
+                    items={hosts.map(host => host._id)} 
+                    strategy={verticalListSortingStrategy}
+                    >
+                    {hosts.map(host => (
+                    <Host key={host._id} {...host} />
+                    )
+                    )
+                    }
+                </SortableContext>
+                
+
+            </DndContext>
         </Container>
     );
 }
@@ -26,4 +54,9 @@ const Container = styled.div`
     heght:100vh;
     margin-left: 30px;
     overflow-y: auto;
+    .Host {
+        margin-bottom: 10px;
+
+        
+    }
 `;
